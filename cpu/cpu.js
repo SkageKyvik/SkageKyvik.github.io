c0692c08a764fd1866fa379139d553311daed332
Skage Kyvik <Skageky@gmail.com>
2019-10-24 08:23:59 +0200

    qq

---
+++ cpu/cpu.js
@@ -0,0 +1,13 @@
+function setup() {
+    let skisse = document.getElementById("skisse");
+    skisse.addEventListener("click", showme);
+
+    function showme(e) {
+        let u = e.target;
+        if (u.dataset && u.dataset.more) {
+            location.href = u.dataset.more + ".html";
+        }
+
+    }
+
+}
